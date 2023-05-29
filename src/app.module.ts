fdeb95e24a4b01e3c625ce8c42ee3c86d7ff9ba4
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-29 12:17:17 +0500

    modules imported app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CarsController } from './cars/cars.controller';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, CarsModule],
+  controllers: [AppController, CarsController],
   providers: [AppService],
 })
 export class AppModule {}
