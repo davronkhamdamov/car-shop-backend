22c2d2ffbc39cdb0bfbd4c50440668052776a37f
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-31 17:19:30 +0500

    car type updated

--- src/cars/interface/car.interface.ts
+++ src/cars/interface/car.interface.ts
@@ -2,6 +2,17 @@ interface ICar {
   id?: string;
   title: string;
   price: string;
+  motor: string;
+  color: string;
+  gearbook: string;
+  deseription: string;
+  tanirovkasi: boolean;
+  year: string;
+  distance: string;
+  baseImgUrl: string;
+  insideImgUrl: string;
+  outsideImgUrl: string;
+  modelId: string;
 }
 
 export { ICar };
