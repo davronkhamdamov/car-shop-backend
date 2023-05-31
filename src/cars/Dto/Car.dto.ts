2e9f5f1d69b06773d8cbc38ef4d02d79c714e30c
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-31 17:44:52 +0500

    cars documantation complate

--- src/cars/Dto/Car.dto.ts
+++ src/cars/Dto/Car.dto.ts
@@ -1,23 +1,100 @@
-import { OmitType, ApiProperty, PickType } from '@nestjs/swagger';
-import { ICar } from '../interface/car.interface';
+import { OmitType, ApiProperty, PickType } from "@nestjs/swagger";
+import { ICar } from "../interface/car.interface";
 
 class ICarDto implements ICar {
   @ApiProperty({
     type: String,
-    default: 'uuid',
+    default: "uuid",
   })
   id?: string;
+
   @ApiProperty({
     type: String,
-    default: 'Malibu',
+    default: "Malibu",
   })
   title: string;
+
   @ApiProperty({
     type: String,
-    default: '1000$',
+    default: "1000$",
   })
   price: string;
+
+  @ApiProperty({
+    type: String,
+    description: "We recommend 200x200 images",
+    default: "https://picsum.photos/300/300",
+  })
+  image: string;
+
+  @ApiProperty({
+    type: String,
+    default: "1.6",
+  })
+  motor: string;
+
+  @ApiProperty({
+    type: String,
+    default: "Black",
+  })
+  color: string;
+
+  @ApiProperty({
+    type: String,
+    default: "Avtomat karobka",
+  })
+  gearbook: string;
+
+  @ApiProperty({
+    type: String,
+    default: "Some text about the car",
+  })
+  deseription: string;
+
+  @ApiProperty({
+    type: String,
+    default: true,
+  })
+  tanirovkasi: boolean;
+
+  @ApiProperty({
+    type: String,
+    default: "2018",
+  })
+  year: string;
+
+  @ApiProperty({
+    type: String,
+    default: "3000km",
+  })
+  distance: string;
+
+  @ApiProperty({
+    type: String,
+    default: "https://picsum.photos/300/300",
+  })
+  baseImgUrl: string;
+
+  @ApiProperty({
+    type: String,
+    description: "We recommend 200x200 images",
+    default: "https://picsum.photos/300/300",
+  })
+  insideImgUrl: string;
+
+  @ApiProperty({
+    type: String,
+    description: "We recommend 200x200 images",
+    default: "https://picsum.photos/300/300",
+  })
+  outsideImgUrl: string;
+
+  @ApiProperty({
+    type: String,
+    default: "uuid",
+  })
+  modelId: string;
 }
-export class CreateCarDto extends OmitType(ICarDto, ['id']) {}
+export class CreateCarDto extends OmitType(ICarDto, ["id"]) {}
 export class AllCarDto extends ICarDto {}
-export class ICarId extends PickType(ICarDto, ['id']) {}
+export class ICarId extends PickType(ICarDto, ["id"]) {}
--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -7,54 +7,64 @@ import {
   Post,
   Put,
   UseGuards,
-} from '@nestjs/common';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CarsService } from './cars.service';
-import { CreateCarDto, ICarId, AllCarDto } from './Dto/Car.dto';
-import { AuthorizationGuard } from 'src/auth/guard/Auth.guard';
-import { CurrentUser } from '../auth/guard/auth.decorator';
-import { CurrentUserDto } from 'src/auth/dto/Corrent.userDto';
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { CarsService } from "./cars.service";
+import { CreateCarDto, ICarId, AllCarDto } from "./Dto/Car.dto";
+import { AuthorizationGuard } from "src/auth/guard/Auth.guard";
+import { CurrentUser } from "../auth/guard/auth.decorator";
+import { CurrentUserDto } from "src/auth/dto/Corrent.userDto";
 
 @ApiBearerAuth()
-@ApiTags('Car')
-@Controller('car')
+@ApiTags("Car")
+@Controller("car")
 @UseGuards(AuthorizationGuard)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
   @ApiResponse({
     status: 200,
-    description: 'All cars will be returned',
+    description: "All cars will be returned",
     type: [AllCarDto],
   })
-  @Get('all')
-  async AllCar() {
+  @Get(":id")
+  async oneCar(@Param() id: ICarId) {
     return this.carsService.getAllCar();
   }
   @ApiResponse({
-    status: 201,
-    description: 'Car create',
+    status: 200,
+    description: "All cars will be returned",
     type: [AllCarDto],
   })
-  @Post('create')
+  @Get("all")
+  async AllCar() {
+    return this.carsService.getAllCar();
+  }
+
+  @ApiResponse({
+    status: 201,
+    description: "Car create",
+    type: [AllCarDto],
+  })
+  @Post("create")
   async createCar(
     @Body() car: CreateCarDto,
-    @CurrentUser() userDI: CurrentUserDto,
+    @CurrentUser() userDI: CurrentUserDto
   ) {
     return this.carsService.createCar(car, userDI);
   }
   @ApiResponse({
     status: 201,
-    description: 'Car update',
+    description: "Car update",
   })
-  @Put('update')
+  @Put("update")
   async UpdateCar(@Body() car: CreateCarDto, @Param() id: ICarId) {
     return this.carsService.UpdateCar(car, id);
   }
   @ApiResponse({
     status: 200,
-    description: 'Car delete',
+    description: "Car delete",
   })
-  @Delete('delete')
+  @Delete("delete")
   async DeleteCar(@Body() car: CreateCarDto, @Param() id: ICarId) {
     return this.carsService.DeleteCar(car, id);
   }
