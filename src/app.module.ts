8bc2488198dc232975db4eb4307439ae65f665e7
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-31 15:45:50 +0500

    jwt imported to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { CarsController } from './cars/cars.controller';
 import { CarsModule } from './cars/cars.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, CarsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      global: true,
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+    AuthModule,
+    CarsModule,
+  ],
 })
 export class AppModule {}
